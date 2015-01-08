8dc5bcad8a3a0035226159645ebbb86846f3a2b9
Yukineko <yukineko.mew7@gmail.com>
2015-01-09 04:17:52 +0900

    First Commit

---
+++ Assets/TimeController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeController : MonoBehaviour {
+	/*
+	public GameObject gameover;
+	public static int time = 0;
+
+	private void start() {;
+		time = 0;
+	}
+
+	private void Update() {
+		int theTime = time - (int)Time.time;
+		if (theTime >= 0) {
+			guiText.text = "Time = " + theTime;
+		}
+
+		if (theTime <= 0) {
+			Instantiate(gameover);
+		}
+
+		if(theTime < -3 ) {
+			Application.LoadLevel("Title");
+		}
+	}
+	*/
+}
---
+++ Assets/warpController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class warpController : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.CompareTag ("warp")) {
+			transform.position = new Vector3(0, 1, 0);
+		}
+	}
+}
